085429c052e6b55764bc9a381a69ac862801a7b9
donnie-jp <32665810+donnie-jp@users.noreply.github.com>
2017-12-14 10:10:38 +0900

    feat(swizzling): REM-24817 Rewrite WKWebView swizzling (#14)
    - use IMP blocks for swizzling (instead of the approach where new selector methods were created on categories) so that selectors are unchanged
    - move the force tracking check to before init’s disable tracking call
    - remove unit tests that relied on the old selector IMP swapping approach
    - remove WKWebView swizzling category because it is incompatible with the new approach
    - fix pragma typos in tests source
    - remove doxygen `Standard metrics` group because we don't have exposed standard metrics now

--- RPerformanceTracking/Private/_RPTClassManipulator+WKWebView.m
+++ RPerformanceTracking/Private/_RPTClassManipulator+WKWebView.m
@@ -1,98 +1,232 @@
 #import "_RPTClassManipulator+WKWebView.h"
-#import "WKWebView+RPerformanceTracking.h"
 #import <WebKit/WebKit.h>
+#import <objc/runtime.h>
+#import "_RPTTrackingManager.h"
+#import "_RPTTracker.h"
+#import "_RPTHelpers.h"
+#import "_RPTMeasurement.h"
+#import "_RPTRingBuffer.h"
+
+static const void *_RPT_WKWEBVIEW_TRACKINGIDENTIFIER = &_RPT_WKWEBVIEW_TRACKINGIDENTIFIER;
+
+static void startTrackingWithWKWebView(WKWebView *webView)
+{
+    [_RPTTrackingManager.sharedInstance.tracker prolongMetric];
+
+    uint_fast64_t trackingIdentifier = [objc_getAssociatedObject(webView, _RPT_WKWEBVIEW_TRACKINGIDENTIFIER) unsignedLongLongValue];
+    
+    NSURL *webViewURL = webView.URL;
+    if (!trackingIdentifier && webViewURL)
+    {
+        trackingIdentifier = [_RPTTrackingManager.sharedInstance.tracker startRequest:[NSURLRequest requestWithURL:webViewURL]];
+        if (trackingIdentifier)
+        {
+            objc_setAssociatedObject(webView, _RPT_WKWEBVIEW_TRACKINGIDENTIFIER, [NSNumber numberWithUnsignedLongLong:trackingIdentifier], OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+        }
+    }
+}
+
+static void endTrackingWithWKWebView(WKWebView *webView)
+{
+    [_RPTTrackingManager.sharedInstance.tracker prolongMetric];
+    [_RPTTrackingManager.sharedInstance.tracker endMetric];
+    
+    uint_fast64_t trackingIdentifier = [objc_getAssociatedObject(webView, _RPT_WKWEBVIEW_TRACKINGIDENTIFIER) unsignedLongLongValue];
+    if (trackingIdentifier)
+    {
+        [_RPTTrackingManager.sharedInstance.tracker end:trackingIdentifier];
+    }
+    objc_setAssociatedObject(webView, _RPT_WKWEBVIEW_TRACKINGIDENTIFIER, nil, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
 
 @implementation _RPTClassManipulator (WKWebView)
-
-/*
- * The methods which are swizzled in "+(void)load" method are still ok, because they are done only once during the application lifecycle.
- */
 + (void)load
 {
-    [_RPTClassManipulator addMethodFromClass:WKWebView.class
-                                withSelector:@selector(_rpt_loadRequest:)
-                                     toClass:WKWebView.class
-                                   replacing:@selector(loadRequest:)
-                               onlyIfPresent:NO];
-
-    [_RPTClassManipulator addMethodFromClass:self
-                                withSelector:@selector(_rpt_setUIDelegate:)
-                                     toClass:WKWebView.class
-                                   replacing:@selector(setUIDelegate:)
-                               onlyIfPresent:YES];
-    
-    [_RPTClassManipulator addMethodFromClass:self
-                                withSelector:@selector(_rpt_setNavigationDelegate:)
-                                     toClass:WKWebView.class
-                                   replacing:@selector(setNavigationDelegate:)
-                               onlyIfPresent:YES];
+    [self _swizzleWKWebView];
 }
 
-/*
- * The methods in the WKUIDelegate and WKNavigationDelegate have to be swizzled only once for each delegate.
- */
-- (void)_rpt_setUIDelegate:(id<WKUIDelegate>)delegate
++ (void)_swizzleWKWebView
 {
-    if (!delegate) return;
-    Class recipient = delegate.class;
-
-    [_RPTClassManipulator swizzleMethodFromClass:WKWebView.class
-                                    withSelector:@selector(_rpt_webView:createWebViewWithConfiguration:forNavigationAction:windowFeatures:)
-                                         toClass:recipient
-                                       replacing:@selector(webView:createWebViewWithConfiguration:forNavigationAction:windowFeatures:)
-                                   onlyIfPresent:NO];
+    // MARK: WKWebView loadRequest:
     
-    [self _rpt_setUIDelegate:delegate];
+    /* The block params below are `self` and then the parameters of the swizzled method.
+     * Unlike Obj-C method calls a _cmd selector is not passed to a block.
+     */
+    id loadRequest_swizzle_blockImp = ^id (id selfRef, NSURLRequest *request) {
+        RPTLog(@"loadRequest_swizzle_blockImp called");
+        
+        if (request.URL) { [[_RPTTrackingManager sharedInstance].tracker prolongMetric]; }
+        
+        SEL selector = @selector(loadRequest:);
+        IMP originalImp = [_RPTClassManipulator implementationForOriginalSelector:selector];
+        
+        if (originalImp)
+        {
+            /* ARC requires casting the IMP to the same signature as the original method.
+             * loadRequest: returns a WKNavigation so the return type in the cast is `id`.
+             * loadRequest: param is a `NSURLRequest` so the full param types are the implicit
+             * `id` (selfRef) and `SEL` (selector), and the explicit `NSURLRequest` (request).
+             *
+             * If the method was void no return would be necessary and the return type would be
+             * `void` not `id`.
+             */
+            return ((id(*)(id, SEL, id))originalImp)(selfRef, selector, request);
+        }
+        return nil;
+    };
+    
+    /* The type characters below are:
+     * first - return type of method, commonly `@` for object or `v` for void
+     * second - `@` for implicit self object parameter
+     * third - `:` for implicit _cmd method selector parameter
+     * after - depends on parameter type
+     */
+    const char *methodTypes = "@@:@";
+    
+    [self swizzleSelector:@selector(loadRequest:)
+                  onClass:WKWebView.class
+        newImplementation:imp_implementationWithBlock(loadRequest_swizzle_blockImp)
+                    types:methodTypes];
+    
+    //  MARK: WKWebView setNavigationDelegate:
+    id setNavDelegate_swizzle_blockImp = ^void (id selfRef, id<WKNavigationDelegate> delegate) {
+        RPTLog(@"setNavDelegate_swizzle_blockImp called");
+        
+        SEL selector = @selector(setNavigationDelegate:);
+        IMP originalImp = [_RPTClassManipulator implementationForOriginalSelector:selector];
+        if (originalImp)
+        {
+            ((void(*)(id, SEL, id))originalImp)(selfRef, selector, delegate);
+            if (delegate)
+            {
+                [_RPTClassManipulator _swizzleWKWebViewNavDelegate:delegate];
+            }
+        }
+    };
+    [self swizzleSelector:@selector(setNavigationDelegate:)
+                  onClass:WKWebView.class
+        newImplementation:imp_implementationWithBlock(setNavDelegate_swizzle_blockImp)
+                    types:"v@:@"];
 }
 
-- (void)_rpt_setNavigationDelegate:(id<WKNavigationDelegate>)delegate
++ (void)_swizzleWKWebViewNavDelegate:(id<WKNavigationDelegate>)delegate
 {
-    if (!delegate) return;
     Class recipient = delegate.class;
-
-    [_RPTClassManipulator swizzleMethodFromClass:WKWebView.class
-                                    withSelector:@selector(_rpt_webView:decidePolicyForNavigationAction:decisionHandler:)
-                                         toClass:recipient
-                                       replacing:@selector(webView:decidePolicyForNavigationAction:decisionHandler:)
-                                   onlyIfPresent:NO];
-
-    [_RPTClassManipulator swizzleMethodFromClass:WKWebView.class
-                                    withSelector:@selector(_rpt_webView:didStartProvisionalNavigation:)
-                                         toClass:recipient
-                                       replacing:@selector(webView:didStartProvisionalNavigation:)
-                                   onlyIfPresent:NO];
-
-    [_RPTClassManipulator swizzleMethodFromClass:WKWebView.class
-                                    withSelector:@selector(_rpt_webView:didFailNavigation:withError:)
-                                         toClass:recipient
-                                       replacing:@selector(webView:didFailNavigation:withError:)
-                                   onlyIfPresent:NO];
-
-    [_RPTClassManipulator swizzleMethodFromClass:WKWebView.class
-                                    withSelector:@selector(_rpt_webView:didFinishNavigation:)
-                                         toClass:recipient
-                                       replacing:@selector(webView:didFinishNavigation:)
-                                   onlyIfPresent:NO];
-
-    [_RPTClassManipulator swizzleMethodFromClass:WKWebView.class
-                                    withSelector:@selector(_rpt_webView:didFailProvisionalNavigation:withError:)
-                                         toClass:recipient
-                                       replacing:@selector(webView:didFailProvisionalNavigation:withError:)
-                                   onlyIfPresent:NO];
-
-    [_RPTClassManipulator swizzleMethodFromClass:WKWebView.class
-                                    withSelector:@selector(_rpt_webViewWebContentProcessDidTerminate:)
-                                         toClass:recipient
-                                       replacing:@selector(webViewWebContentProcessDidTerminate:)
-                                   onlyIfPresent:NO];
-
-    [_RPTClassManipulator swizzleMethodFromClass:WKWebView.class
-                                    withSelector:@selector(_rpt_webView:didReceiveServerRedirectForProvisionalNavigation:)
-                                         toClass:recipient
-                                       replacing:@selector(webView:didReceiveServerRedirectForProvisionalNavigation:)
-                                   onlyIfPresent:NO];
-
-    [self _rpt_setNavigationDelegate:delegate];
+    
+    // MARK: WKNavigationDelegate webView:didStartProvisionalNavigation:
+    id didStart_swizzle_blockImp = ^(id selfRef, WKWebView *webView, WKNavigation *navigation) {
+        RPTLog(@"didStart_swizzle_blockImp called");
+        
+        SEL selector = @selector(webView:didStartProvisionalNavigation:);
+        IMP originalImp = [_RPTClassManipulator implementationForOriginalSelector:selector];
+        if (originalImp)
+        {
+            ((void(*)(id, SEL, id, id))originalImp)(selfRef, selector, webView, navigation);
+        }
+        
+        startTrackingWithWKWebView(webView);
+    };
+    [self swizzleSelector:@selector(webView:didStartProvisionalNavigation:)
+                  onClass:recipient
+        newImplementation:imp_implementationWithBlock(didStart_swizzle_blockImp)
+                    types:"v@:@@"];
+    
+    // MARK: WKNavigationDelegate webView:didFinishNavigation:
+    id didFinishNavigation_swizzle_blockImp = ^(id selfRef, WKWebView *webView, WKNavigation *navigation) {
+        RPTLog(@"didFinishNavigation_swizzle_blockImp called");
+        
+        SEL selector = @selector(webView:didFinishNavigation:);
+        IMP originalImp = [_RPTClassManipulator implementationForOriginalSelector:selector];
+        if (originalImp)
+        {
+            ((void(*)(id, SEL, id, id))originalImp)(selfRef, selector, webView, navigation);
+        }
+        
+        endTrackingWithWKWebView(webView);
+    };
+    [self swizzleSelector:@selector(webView:didFinishNavigation:)
+                  onClass:recipient
+        newImplementation:imp_implementationWithBlock(didFinishNavigation_swizzle_blockImp)
+                    types:"v@:@@"];
+    
+    // MARK: WKNavigationDelegate webView:didFailNavigation:withError:
+    id didFailNavigation_swizzle_blockImp = ^(id selfRef, WKWebView *webView, WKNavigation *navigation, NSError *error) {
+        RPTLog(@"didFailNavigation_swizzle_blockImp called");
+        
+        SEL selector = @selector(webView:didFailNavigation:withError:);
+        IMP originalImp = [_RPTClassManipulator implementationForOriginalSelector:selector];
+        if (originalImp)
+        {
+            ((void(*)(id, SEL, id, id, id))originalImp)(selfRef, selector, webView, navigation, error);
+        }
+        
+        endTrackingWithWKWebView(webView);
+    };
+    [self swizzleSelector:@selector(webView:didFailNavigation:withError:)
+                  onClass:recipient
+        newImplementation:imp_implementationWithBlock(didFailNavigation_swizzle_blockImp)
+                    types:"v@:@@@"];
+    
+    // MARK: WKNavigationDelegate webView:didFailProvisionalNavigation:withError:
+    id didFailProvisionalNavigation_swizzle_blockImp = ^(id selfRef, WKWebView *webView, WKNavigation *navigation, NSError *error) {
+        RPTLog(@"didFailProvisionalNavigation_swizzle_blockImp called");
+        
+        SEL selector = @selector(webView:didFailProvisionalNavigation:withError:);
+        IMP originalImp = [_RPTClassManipulator implementationForOriginalSelector:selector];
+        if (originalImp)
+        {
+            ((void(*)(id, SEL, id, id, id))originalImp)(selfRef, selector, webView, navigation, error);
+        }
+        
+        endTrackingWithWKWebView(webView);
+    };
+    [self swizzleSelector:@selector(webView:didFailProvisionalNavigation:withError:)
+                  onClass:recipient
+        newImplementation:imp_implementationWithBlock(didFailProvisionalNavigation_swizzle_blockImp)
+                    types:"v@:@@@"];
+    
+    // MARK: WKNavigationDelegate webViewWebContentProcessDidTerminate:
+    id webContentProcessDidTerminate_swizzle_blockImp = ^(id selfRef, WKWebView *webView) {
+        RPTLog(@"webContentProcessDidTerminate_swizzle_blockImp called");
+        
+        SEL selector = @selector(webViewWebContentProcessDidTerminate:);
+        IMP originalImp = [_RPTClassManipulator implementationForOriginalSelector:selector];
+        if (originalImp)
+        {
+            ((void(*)(id, SEL, id))originalImp)(selfRef, selector, webView);
+        }
+        
+        endTrackingWithWKWebView(webView);
+    };
+    [self swizzleSelector:@selector(webViewWebContentProcessDidTerminate:)
+                  onClass:recipient
+        newImplementation:imp_implementationWithBlock(webContentProcessDidTerminate_swizzle_blockImp)
+                    types:"v@:@"];
+    
+    // MARK: WKNavigationDelegate webView:didReceiveServerRedirectForProvisionalNavigation:
+    id didReceiveServerRedirect_swizzle_blockImp = ^(id selfRef, WKWebView *webView, WKNavigation *navigation) {
+        RPTLog(@"didReceiveServerRedirect_swizzle_blockImp called");
+        
+        SEL selector = @selector(webView:didReceiveServerRedirectForProvisionalNavigation:);
+        IMP originalImp = [_RPTClassManipulator implementationForOriginalSelector:selector];
+        if (originalImp)
+        {
+            ((void(*)(id, SEL, id, id))originalImp)(selfRef, selector, webView, navigation);
+        }
+        
+        if (webView.URL)
+        {
+            uint_fast64_t trackingIdentifier = [objc_getAssociatedObject(webView, _RPT_WKWEBVIEW_TRACKINGIDENTIFIER) unsignedLongLongValue];
+            if (trackingIdentifier)
+            {
+                _RPTMeasurement *measurement = [[_RPTTrackingManager sharedInstance].ringBuffer measurementWithTrackingIdentifier:trackingIdentifier];
+                measurement.receiver = webView.URL.absoluteString;
+            }
+        }
+    };
+    [self swizzleSelector:@selector(webView:didReceiveServerRedirectForProvisionalNavigation:)
+                  onClass:recipient
+        newImplementation:imp_implementationWithBlock(didReceiveServerRedirect_swizzle_blockImp)
+                    types:"v@:@@"];
 }
-
 @end
--- Tests/WKWebViewHookTests.m
+++ Tests/WKWebViewHookTests.m
@@ -10,23 +10,17 @@
 #import "_RPTSender.h"
 #import <WebKit/WebKit.h>
 #import "_RPTClassManipulator.h"
-#import "WKWebView+RPerformanceTracking.h"
 
 @interface _RPTTrackingManager ()
 @property (nonatomic) _RPTTracker    *tracker;
 @property (nonatomic) _RPTRingBuffer *ringBuffer;
 @end
 
-@interface _RPTClassManipulator ()
-- (void)_rpt_setNavigationDelegate:(id<WKNavigationDelegate>)delegate;
-- (void)_rpt_setUIDelegate:(id<WKUIDelegate>)delegate;
-@end
-
 @interface _RPTTracker ()
 @property (atomic) _RPTMetric *currentMetric;
 @end
 
-@interface WKWebViewHookTests : XCTestCase <WKUIDelegate, WKNavigationDelegate>
+@interface WKWebViewHookTests : XCTestCase <WKNavigationDelegate>
 @property (strong, nonatomic) WKWebView *webView;
 @end
 
@@ -40,95 +34,23 @@ static _RPTTrackingManager *_trackingManager = nil;
     
     _trackingManager                       = [_RPTTrackingManager sharedInstance];
     _RPTRingBuffer *ringBuffer             = [_RPTRingBuffer.alloc initWithSize:12];
-    _trackingManager.ringBuffer             = ringBuffer;
+    _trackingManager.ringBuffer            = ringBuffer;
     _RPTMetric *currentMetric              = _RPTMetric.new;
     currentMetric.identifier               = @"metric";
-    _trackingManager.tracker                = [_RPTTracker.alloc initWithRingBuffer:ringBuffer
-                                                                   currentMetric:currentMetric];
+    _trackingManager.tracker               = [_RPTTracker.alloc initWithRingBuffer:ringBuffer
+                                                                     currentMetric:currentMetric];
     _webView                               = [[WKWebView alloc] initWithFrame:CGRectZero
                                                                 configuration:[[WKWebViewConfiguration alloc] init]];
+    _webView.navigationDelegate            = self;
     [_webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"www.google.com"]]];
-    _webView.navigationDelegate              = self;
-    _webView.UIDelegate                     = self;
 }
 
 - (void)tearDown
 {
-    Class recipient = _webView.navigationDelegate.class;
-    [_RPTClassManipulator addMethodFromClass:recipient
-                                withSelector:@selector(webView:decidePolicyForNavigationAction:decisionHandler:)
-                                     toClass:WKWebView.class
-                                   replacing:@selector(_rpt_webView:decidePolicyForNavigationAction:decisionHandler:)
-                               onlyIfPresent:NO];
-    
-    [_RPTClassManipulator addMethodFromClass:recipient
-                                withSelector:@selector(webView:didStartProvisionalNavigation:)
-                                     toClass:WKWebView.class
-                                   replacing:@selector(_rpt_webView:didStartProvisionalNavigation:)
-                               onlyIfPresent:NO];
-    
-    [_RPTClassManipulator addMethodFromClass:recipient
-                                withSelector:@selector(webView:didFailNavigation:withError:)
-                                     toClass:WKWebView.class
-                                   replacing:@selector(_rpt_webView:didFailNavigation:withError:)
-                               onlyIfPresent:NO];
-    
-    [_RPTClassManipulator addMethodFromClass:recipient
-                                withSelector:@selector(webView:didFailProvisionalNavigation:withError:)
-                                     toClass:WKWebView.class
-                                   replacing:@selector(_rpt_webView:didFailProvisionalNavigation:withError:)
-                               onlyIfPresent:NO];
-    
-    [_RPTClassManipulator addMethodFromClass:recipient
-                                withSelector:@selector(webView:didFinishNavigation:)
-                                     toClass:WKWebView.class
-                                   replacing:@selector(_rpt_webView:didFinishNavigation:)
-                               onlyIfPresent:NO];
-    
-    [_RPTClassManipulator addMethodFromClass:_webView.UIDelegate.class
-                                withSelector:@selector(webView:createWebViewWithConfiguration:forNavigationAction:windowFeatures:)
-                                     toClass:WKWebView.class
-                                   replacing:@selector(_rpt_webView:createWebViewWithConfiguration:forNavigationAction:windowFeatures:)
-                               onlyIfPresent:NO];
-    
     _webView.navigationDelegate = nil;
-    _webView.UIDelegate   = nil;
 }
 
-- (void)testSwizzlingMethodLoadRequestIsAddedToWKWebViewClass
-{
-    XCTAssert([_webView respondsToSelector:@selector(_rpt_loadRequest:)]);
-}
-
-- (void)testOriginalMethodLoadRequestIsPresentInWKWebviewClass
-{
-    XCTAssert([_webView respondsToSelector:@selector(loadRequest:)]);
-}
-
-- (void)testSwizzlingMethodNavigationActionIsAddedToWKWebViewClass
-{
-    XCTAssert([_webView respondsToSelector:@selector(_rpt_webView:createWebViewWithConfiguration:forNavigationAction:windowFeatures:)]);
-}
-
-- (void)testSwizzlingMethodSetNavigationDelegateIsAddedToWKWebViewClass
-{
-    XCTAssert([_webView respondsToSelector:@selector(_rpt_setNavigationDelegate:)]);
-}
-
-- (void)testOriginalMethodSetNavigationDelegateIsPresentInWKWebviewClass
-{
-    XCTAssert([_webView respondsToSelector:@selector(setNavigationDelegate:)]);
-}
-
-- (void)testSwizzlingMethodSetUIDelegateIsAddedToWKWebViewClass
-{
-    XCTAssert([_webView respondsToSelector:@selector(_rpt_setUIDelegate:)]);
-}
-
-- (void)testOriginalMethodUIDelegateIsPresentInWKWebviewClass
-{
-    XCTAssert([_webView respondsToSelector:@selector(setUIDelegate:)]);
-}
+// MARK: Tests to verify the side effects of our swizzles e.g. has the expected _RPTTracker method been called
 
 - (void)testIfNavigationDelegateIsNotNil
 {
@@ -139,17 +61,6 @@ static _RPTTrackingManager *_trackingManager = nil;
 {
     id mockTracker = OCMPartialMock(_trackingManager.tracker);
     [_webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"www.google.com"]]];
-    OCMVerify([mockTracker prolongMetric]);
-    [mockTracker stopMocking];
-}
-
-- (void)testProlongMetricCalledOnWKWebViewDecidePolicyForNavigationAction
-{
-    id mockTracker = OCMPartialMock(_trackingManager.tracker);
-    [_webView.navigationDelegate webView:_webView
-         decidePolicyForNavigationAction:[WKNavigationAction new]
-                         decisionHandler:^(WKNavigationActionPolicy policy) {
-    }];
     OCMVerify([mockTracker prolongMetric]);
     [mockTracker stopMocking];
 }
@@ -207,6 +118,7 @@ static _RPTTrackingManager *_trackingManager = nil;
     OCMVerify([mockTracker prolongMetric]);
     [mockTracker stopMocking];
 }
+
 - (void)testEndMetricCalledOnWKWebviewDidFailProvisionalNavigation
 {
     id mockTracker = OCMPartialMock(_trackingManager.tracker);
@@ -216,29 +128,27 @@ static _RPTTrackingManager *_trackingManager = nil;
     OCMVerify([mockTracker endMetric]);
     [mockTracker stopMocking];
 }
-#pragma MARK: WKNavigationDelegate
+
+// MARK: WKNavigationDelegate
 
 - (void)webView:(WKWebView *)webView decidePolicyForNavigationAction:(WKNavigationAction *)navigationAction decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler
 {
 }
+
 - (void)webView:(WKWebView *)webView didStartProvisionalNavigation:(WKNavigation *)navigation
 {
 }
+
 - (void)webView:(WKWebView *)webView didFailNavigation:(WKNavigation *)navigation withError:(NSError *)error
 {
 }
+
 - (void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation
 {
 }
+
 - (void)webView:(WKWebView *)webView didFailProvisionalNavigation:(WKNavigation *)navigation withError:(NSError *)error
 {
 }
-
-#pragma MARK: WKUIDelegate
-- (WKWebView *)webView:(WKWebView *)webView createWebViewWithConfiguration:(WKWebViewConfiguration *)configuration forNavigationAction:(WKNavigationAction *)navigationAction windowFeatures:(WKWindowFeatures *)windowFeatures
-{
-    return [WKWebView new];
-}
-
 
 @end
