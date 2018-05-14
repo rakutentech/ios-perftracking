4713527fc013bd7e723f94712d7eb52239eea272
vito1188 <viet.ta@rakuten.com>
2018-05-14 11:56:31 +0900

    Fix code reviews and rebase on APTT-27

--- RPerformanceTracking/Private/_RPTTrackingManager.m
+++ RPerformanceTracking/Private/_RPTTrackingManager.m
@@ -10,7 +10,6 @@
 #import "_RPTLocation.h"
 #import "_RPTMainThreadWatcher.h"
 #import "_RPTEnvironment.h"
-#import "_RPTClassManipulator.h"
 
 static const NSUInteger      MAX_MEASUREMENTS               = 512u;
 static const NSUInteger      TRACKING_DATA_LIMIT            = 100u;
