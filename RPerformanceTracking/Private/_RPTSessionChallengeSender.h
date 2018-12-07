acf19113067af4b5e47933014e8f536bbafc2463
Smith, Donnie <donnie.smith@rakuten.com>
2018-12-07 12:57:27 +0900

    tests: add missing WKWebView tests, remove unused SessionChallengeSender class

--- RPerformanceTracking/Private/_RPTSessionChallengeSender.h
+++
@@ -1,7 +0,0 @@
-#import <RPerformanceTracking/RPTDefines.h>
-
-@interface _RPTSessionChallengeSender : NSObject <NSURLAuthenticationChallengeSender>
-
-- (instancetype)initWithSessionCompletionHandler:(void (^)(NSURLSessionAuthChallengeDisposition disposition, NSURLCredential *credential))completionHandler;
-
-@end
